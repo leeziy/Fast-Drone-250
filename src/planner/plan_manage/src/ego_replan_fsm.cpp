4cf20bb4fdd89764b4f0e0cf1436c14c14c7b554
李泽颖 <lizy48@foxmail.com>
2025-05-31 23:24:22 +0800

    Update ego_replan_fsm.cpp

--- src/planner/plan_manage/src/ego_replan_fsm.cpp
+++ src/planner/plan_manage/src/ego_replan_fsm.cpp
@@ -217,7 +217,7 @@ namespace ego_planner
     // trigger_ = true;
     init_pt_ = odom_pos_;
 
-    Eigen::Vector3d end_wp(msg->pose.position.x, msg->pose.position.y, 1.0);
+    Eigen::Vector3d end_wp(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
 
     planNextWaypoint(end_wp);
   }
